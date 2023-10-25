a26828d9b14ee6104a3ba3b4e8d79ec1508c38c4
Lyubomir <119952676+BlackRock17@users.noreply.github.com>
2023-10-25 15:07:52 +0300

    data migration

---
+++ Migrations and Django Admin/main_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Shoe(models.Model):
+    brand = models.CharField(
+        max_length=25,
+    )
+
+    size = models.PositiveIntegerField()
+
+
+class UniqueBrands(models.Model):
+    brand_name = models.CharField(
+        max_length=25,
+    )
