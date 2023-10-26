eb33fbb7f6526929237ef0e6f6cfad2b650c500a
Lyubomir <119952676+BlackRock17@users.noreply.github.com>
2023-10-26 20:39:56 +0300

    Data Migrations

--- Migrations and Django Admin/main_app/models.py
+++ Migrations and Django Admin/main_app/models.py
@@ -94,7 +94,67 @@ class Supplier(models.Model):
         return f"{self.name} - {self.phone}"
 
 
+class Course(models.Model):
+    title = models.CharField(
+        max_length=90
+    )
 
+    lecturer = models.CharField(
+        max_length=90
+    )
+
+    description = models.TextField(
+        max_length=200
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    start_date = models.DateField(
+        auto_now_add=True
+    )
+
+    is_published = models.BooleanField(
+        default=True
+    )
+
+    def __str__(self):
+        return f"{self.title} - {self.lecturer}"
+
+
+class Person(models.Model):
+    name = models.CharField(
+        max_length=40
+    )
+
+    age = models.PositiveIntegerField()
+
+    age_group = models.CharField(
+        max_length=20,
+        default='No age group'
+    )
+
+    def __str__(self):
+        return f"Name: {self.name}"
+
+
+class SmartPhone(models.Model):
+    brand = models.CharField(
+        max_length=100
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+
+    category = models.CharField(
+        max_length=20,
+        default='empty'
+    )
 
 
 
