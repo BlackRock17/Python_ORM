ad84f64f23a45188708ba5ab31724359f3ba6bac
Lyubomir <119952676+BlackRock17@users.noreply.github.com>
2023-10-27 23:31:24 +0300

    Data Migrations

--- Migrations and Django Admin/main_app/models.py
+++ Migrations and Django Admin/main_app/models.py
@@ -157,8 +157,74 @@ class SmartPhone(models.Model):
     )
 
 
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    )
+
+    product_name = models.CharField(
+        max_length=30
+    )
+
+    customer_name = models.CharField(
+        max_length=100
+    )
+
+    order_date = models.DateField()
+
+    status = models.CharField(
+        max_length=30,
+        choices=STATUS_CHOICES
+    )
+
+    amount = models.PositiveIntegerField(
+        default=1
+    )
+
+    product_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+
+    warranty = models.CharField(
+        default='No warranty'
+    )
+
+    delivery = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+       return f"Order #{self.id} - {self.customer_name}"
 
 
+class Item(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=1
+    )
+
+    rarity = models.CharField(
+        max_length=20,
+        default='empty'
+    )
 
 
 
