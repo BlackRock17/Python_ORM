6649c54c32941fb94bc8af3d205d33e4c6663030
Lyubomir <119952676+BlackRock17@users.noreply.github.com>
2023-11-21 22:35:14 +0200

    Models, Query and Admin

--- Exam Prep 1/caller.py
+++ Exam Prep 1/caller.py
@@ -1,6 +1,6 @@
 import os
 import django
-from django.db.models import Q, Count
+from django.db.models import Q, Count, Avg
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
@@ -8,7 +8,8 @@ django.setup()
 
 # Import your models here
 
-from main_app.models import Director
+from main_app.models import Director, Actor, Movie
+
 
 # Create and run your queries within functions
 
@@ -22,9 +23,12 @@ def get_directors(search_name=None, search_nationality=None):
     elif search_name:
         query = Q(full_name__icontains=search_name)
     else:
-        return ''
+        query = Q(nationality__icontains=search_nationality)
 
     directors = Director.objects.filter(query).order_by('full_name')
+
+    if not directors:
+        return ''
 
     result = []
 
@@ -39,11 +43,42 @@ def get_directors(search_name=None, search_nationality=None):
 
 
 def get_top_director():
-    director = Director.objects.get_directors_by_movies_count(
-    ).annotate(num_of_movies=Count('movies')
-               ).order_by('-num_of_movies', 'full_name'
-                          ).first()
+    director = Director.objects.get_directors_by_movies_count().first()
 
-    return f"Top Director: {director.full_name}, movies: {director.num_of_movies}."
+    if not director:
+        return ''
 
+    return f"Top Director: {director.full_name}, movies: {director.num_movies}."
+
+
+def get_top_actor():
+    actor = Actor.objects.annotate(starred_in=Count('movies')
+                                   ).order_by('-starred_in', 'full_name').first()
+
+    actor_movies = Movie.objects.filter(starring_actor=actor)
+
+    if not actor or not actor_movies:
+        return ''
+
+    avg_rating = actor_movies.aggregate(avg_rating=Avg('rating'))['avg_rating']
+
+    return (f"Top Actor: {actor.full_name}, starring in movies: {', '.join([movie.title for movie in actor_movies])}, "
+            f"movies average rating: {avg_rating:.1f}")
+
+
+# def get_top_actor():
+#     actor = Actor.objects.prefetch_related('movies') \
+#         .annotate(
+#         num_of_movies=Count('movies'),
+#         movies_avg_rating=Avg('movies__rating')) \
+#         .order_by('-num_of_movies', 'full_name') \
+#         .first()
+#
+#     if not actor or not actor.num_of_movies:
+#         return ""
+#
+#     movies = ", ".join(movie.title for movie in actor.movies.all() if movie)
+#
+#     return f"Top Actor: {actor.full_name}, starring in movies: {movies}, " \
+#            f"movies average rating: {actor.movies_avg_rating:.1f}"
 
